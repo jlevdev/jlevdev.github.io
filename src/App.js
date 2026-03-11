216cb0fc63467dea7f62568eca6dc17f2df1862e
James LeVasseur <jlevdev@gmail.com>
2026-03-11 13:47:42 -0400

    added oxc and biome for formatting and linting, switched to pnpm

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import umobear from "./assets/umologo.png";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCalendar,
-  faEnvelope,
-  faLocationDot,
-  faMobileButton,
-  faMoon,
-  faSun,
+	faCalendar,
+	faEnvelope,
+	faLocationDot,
+	faMobileButton,
+	faMoon,
+	faSun,
 } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
@@ -22,295 +22,287 @@ const DAY_BORDER = `3px solid ${BLACK}`;
 const NIGHT_BORDER = `3px solid ${WHITE}`;
 
 function App() {
-  const currentTime = new Date().getHours();
+	const currentTime = new Date().getHours();
 
-  const initialDayNightSetting =
-    currentTime > 17 || currentTime < 9 ? NIGHT : DAY;
-  const [dayNight, setDayNight] = useState(initialDayNightSetting);
-  const [bodyBG, setbodyBG] = useState(
-    initialDayNightSetting === DAY ? WHITE : SLATE
-  );
-  const [bodyColor, setbodyColor] = useState(
-    initialDayNightSetting === DAY ? BLACK : WHITE
-  );
-  const [h3BorderBottom, setH3BorderBottom] = useState({
-    borderBottom: initialDayNightSetting === DAY ? DAY_BORDER : NIGHT_BORDER,
-  });
+	const initialDayNightSetting =
+		currentTime > 17 || currentTime < 9 ? NIGHT : DAY;
+	const [dayNight, setDayNight] = useState(initialDayNightSetting);
+	const [bodyBG, setbodyBG] = useState(
+		initialDayNightSetting === DAY ? WHITE : SLATE,
+	);
+	const [bodyColor, setbodyColor] = useState(
+		initialDayNightSetting === DAY ? BLACK : WHITE,
+	);
+	const [h3BorderBottom, setH3BorderBottom] = useState({
+		borderBottom: initialDayNightSetting === DAY ? DAY_BORDER : NIGHT_BORDER,
+	});
 
-  document.body.style.background = bodyBG;
-  document.body.style.color = bodyColor;
+	document.body.style.background = bodyBG;
+	document.body.style.color = bodyColor;
 
-  const dayNightSwitch = () => {
-    if (dayNight === NIGHT) {
-      setbodyBG(WHITE);
-      setbodyColor(BLACK);
-      setDayNight(DAY);
-      setH3BorderBottom({ borderBottom: DAY_BORDER });
-    } else {
-      setbodyBG(SLATE);
-      setbodyColor(WHITE);
-      setDayNight(NIGHT);
-      setH3BorderBottom({ borderBottom: NIGHT_BORDER });
-    }
-  };
+	const dayNightSwitch = () => {
+		if (dayNight === NIGHT) {
+			setbodyBG(WHITE);
+			setbodyColor(BLACK);
+			setDayNight(DAY);
+			setH3BorderBottom({ borderBottom: DAY_BORDER });
+		} else {
+			setbodyBG(SLATE);
+			setbodyColor(WHITE);
+			setDayNight(NIGHT);
+			setH3BorderBottom({ borderBottom: NIGHT_BORDER });
+		}
+	};
 
-  return (
-    <div className="App container">
-      <div className="top-buttons">
-        <FontAwesomeIcon
-          data-html2canvas-ignore="true"
-          icon={dayNight === DAY ? faSun : faMoon}
-          onClick={() => {
-            dayNightSwitch();
-          }}
-        />
-      </div>
-      <div className="row">
-        <header className="title col-12">
-          <h1>James LeVasseur</h1>
-          <h2>Developer, Sys Admin, Nerd</h2>
-        </header>
-      </div>
-      <main className="row">
-        <div className="little-bits col-12">
-          <ul>
-            <li>
-              <FontAwesomeIcon icon={faEnvelope} />
-              <a
-                href="mailto:jlevdev@gmail.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                jlevdev@gmail.com
-              </a>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faMobileButton} />
-              <span className="padded">(207) 659-9328</span>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faLocationDot} />
-              <span className="padded">Lisbon Falls, ME</span>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faLinkedin} />
-              <a
-                href="https://www.linkedin.com/in/james-levasseur-7389aa168/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                LinkedIn
-              </a>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faGithub} />
-              <a
-                href="https://github.com/jlevdev"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="left-col col-8">
-          <div className="text-block career-obj" data-html2canvas-ignore="true">
-            <h3 style={h3BorderBottom}>Career Objective</h3>
-            <p>
-              Hi, I'm James and welcome to my site/resume. I'm a tech enthusiast
-              who's gone from IT Support to System Administrator and now
-              Fullstack Developer. If I had to encapsulate my career into a
-              phrase it would be "learn on the go". I have been coding since high school and have taken
-              on a variety of projects both professional and personal. From
-              websites, to microcontrollers, 3D printing, and homelab setups. I
-              learn by doing on the regular and would be happy to help with your
-              next project.
-            </p>
-          </div>
-          <div className="text-block work-exp">
-            <h3 style={h3BorderBottom}>Work Experience</h3>
-            <h4>Fullstack Developer</h4>
-            <h5>Wayside Publishing</h5>
-            <span>
-              <FontAwesomeIcon icon={faCalendar} />
-              <span>2022 - Present</span>
-            </span>
-            <span>
-              <FontAwesomeIcon icon={faLocationDot} />
-              <span>Freeport, ME</span>
-            </span>
+	return (
+		<div className="App container">
+			<div className="top-buttons">
+				<FontAwesomeIcon
+					data-html2canvas-ignore="true"
+					icon={dayNight === DAY ? faSun : faMoon}
+					onClick={() => {
+						dayNightSwitch();
+					}}
+				/>
+			</div>
+			<div className="row">
+				<header className="title col-12">
+					<h1>James LeVasseur</h1>
+					<h2>Developer, Sys Admin, Nerd</h2>
+				</header>
+			</div>
+			<main className="row">
+				<div className="little-bits col-12">
+					<ul>
+						<li>
+							<FontAwesomeIcon icon={faEnvelope} />
+							<a
+								href="mailto:jlevdev@gmail.com"
+								target="_blank"
+								rel="noreferrer"
+							>
+								jlevdev@gmail.com
+							</a>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faMobileButton} />
+							<span className="padded">(207) 659-9328</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faLocationDot} />
+							<span className="padded">Lisbon Falls, ME</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faLinkedin} />
+							<a
+								href="https://www.linkedin.com/in/james-levasseur-7389aa168/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								LinkedIn
+							</a>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faGithub} />
+							<a
+								href="https://github.com/jlevdev"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Github
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className="left-col col-8">
+					<div className="text-block career-obj" data-html2canvas-ignore="true">
+						<h3 style={h3BorderBottom}>Career Objective</h3>
+						<p>
+							Hi, I'm James and welcome to my site/resume. I'm a tech enthusiast
+							who's gone from IT Support to System Administrator and now
+							Fullstack Developer. If I had to encapsulate my career into a
+							phrase it would be "learn on the go". I have been coding since
+							high school and have taken on a variety of projects both
+							professional and personal. From websites, to microcontrollers, 3D
+							printing, and homelab setups. I learn by doing on the regular and
+							would be happy to help with your next project.
+						</p>
+					</div>
+					<div className="text-block work-exp">
+						<h3 style={h3BorderBottom}>Work Experience</h3>
+						<h4>Fullstack Developer</h4>
+						<h5>Wayside Publishing</h5>
+						<span>
+							<FontAwesomeIcon icon={faCalendar} />
+							<span>2022 - Present</span>
+						</span>
+						<span>
+							<FontAwesomeIcon icon={faLocationDot} />
+							<span>Freeport, ME</span>
+						</span>
 
-            <ul>
-              <li>
-                Lead overhaul of WCAG 2.2 compliance in React front-end
-              </li>
-              <li>
-                Developed Power BI connector to integrate with SQL and MongoDB
-              </li>
-              <li>
-                Assisted in major upgrade of back-end framework and CMS
-              </li>
-              <li>
-                Routinely developed new major UI components with React
-              </li>
-              <li>
-                Routinely participated in design and feature review process
-              </li>
-              <li>
-                Participated in on call rotation
-              </li>
-            </ul>
+						<ul>
+							<li>Lead overhaul of WCAG 2.2 compliance in React front-end</li>
+							<li>
+								Developed Power BI connector to integrate with SQL and MongoDB
+							</li>
+							<li>Assisted in major upgrade of back-end framework and CMS</li>
+							<li>Routinely developed new major UI components with React</li>
+							<li>
+								Routinely participated in design and feature review process
+							</li>
+							<li>Participated in on call rotation</li>
+						</ul>
 
-            <h4>Network Administrator</h4>
-            <h5>Wayside Publishing</h5>
-            <span>
-              <FontAwesomeIcon icon={faCalendar} />
-              <span>2017 - 2022</span>
-            </span>
-            <span>
-              <FontAwesomeIcon icon={faLocationDot} />
-              <span>Freeport, ME</span>
-            </span>
+						<h4>Network Administrator</h4>
+						<h5>Wayside Publishing</h5>
+						<span>
+							<FontAwesomeIcon icon={faCalendar} />
+							<span>2017 - 2022</span>
+						</span>
+						<span>
+							<FontAwesomeIcon icon={faLocationDot} />
+							<span>Freeport, ME</span>
+						</span>
 
-            <ul>
-              <li>Maintained Linux servers on premises and in AWS</li>
-              <li>
-                Maintained, upgraded, and archived applications with SQL
-                databases
-              </li>
-              <li>Managed software & inventory budget</li>
-              <li>Managed IT help desk and help desk staff</li>
-              <li>
-                Implemented MDM, AzureAD, and patch management systems for all
-                endpoints
-              </li>
-              <li>
-                Managed adjustments to COVID-19 remote work for entire 100
-                employee staff
-              </li>
-              <li>
-                Upgraded & fortified LAN infrastructure against cyber security
-                incidents
-              </li>
-            </ul>
+						<ul>
+							<li>Maintained Linux servers on premises and in AWS</li>
+							<li>
+								Maintained, upgraded, and archived applications with SQL
+								databases
+							</li>
+							<li>Managed software & inventory budget</li>
+							<li>Managed IT help desk and help desk staff</li>
+							<li>
+								Implemented MDM, AzureAD, and patch management systems for all
+								endpoints
+							</li>
+							<li>
+								Managed adjustments to COVID-19 remote work for entire 100
+								employee staff
+							</li>
+							<li>
+								Upgraded & fortified LAN infrastructure against cyber security
+								incidents
+							</li>
+						</ul>
 
-            <h4>Technical Support Rep</h4>
-            <h5>Wayside Publishing</h5>
-            <span>
-              <FontAwesomeIcon icon={faCalendar} />
-              <span>2017-2018</span>
-            </span>
-            <span>
-              <FontAwesomeIcon icon={faLocationDot} />
-              <span>Freeport, ME</span>
-            </span>
-            <ul>
-              <li>
-                Provided support for Wayside Publishing customers via phone and
-                email
-              </li>
-              <li>
-                Created custom data connector between Salesforce, production web
-                app, and other proprietary systems
-              </li>
-              <li>
-                Worked with Dev Team and client IT admins to automate user
-                onboarding
-              </li>
-              <li>
-                Automated data entry for major projects to meet strict deadlines
-              </li>
-            </ul>
-            <h4>Developer</h4>
-            <h5>ASAP Media Services</h5>
-            <span>
-              <FontAwesomeIcon icon={faCalendar} />
-              <span>2014-2015</span>
-            </span>
-            <span>
-              <FontAwesomeIcon icon={faLocationDot} />
-              <span>Orono, ME</span>
-            </span>
+						<h4>Technical Support Rep</h4>
+						<h5>Wayside Publishing</h5>
+						<span>
+							<FontAwesomeIcon icon={faCalendar} />
+							<span>2017-2018</span>
+						</span>
+						<span>
+							<FontAwesomeIcon icon={faLocationDot} />
+							<span>Freeport, ME</span>
+						</span>
+						<ul>
+							<li>
+								Provided support for Wayside Publishing customers via phone and
+								email
+							</li>
+							<li>
+								Created custom data connector between Salesforce, production web
+								app, and other proprietary systems
+							</li>
+							<li>
+								Worked with Dev Team and client IT admins to automate user
+								onboarding
+							</li>
+							<li>
+								Automated data entry for major projects to meet strict deadlines
+							</li>
+						</ul>
+						<h4>Developer</h4>
+						<h5>ASAP Media Services</h5>
+						<span>
+							<FontAwesomeIcon icon={faCalendar} />
+							<span>2014-2015</span>
+						</span>
+						<span>
+							<FontAwesomeIcon icon={faLocationDot} />
+							<span>Orono, ME</span>
+						</span>
 
-            <ul>
-              <li>
-                Front end and back end developer using PHP, JavaScript, CSS, and
-                HTML.
-              </li>
-              <li>Developed user interfaces in Objective C for iOS</li>
-              <li>Met with key stakeholders to document needs for projects</li>
-              <li>Worked on 3+ projects simultaneously</li>
-              <li>Designed and implemented complex user interfaces</li>
-            </ul>
-          </div>
-        </div>
-        <div className="right-col col-4">
-          <div className="text-block">
-            <h3 style={h3BorderBottom}>Education</h3>
-            <p>Bachelors in New Media</p>
-            <p>Minor in Computer Science, Film &amp; Video</p>
-            <p>
-              <img src={umobear} width="15" alt="UMO logo" /> University of
-              Maine
-            </p>
-            <p>
-              <span>
-                <FontAwesomeIcon icon={faLocationDot} /> <span> Orono, ME</span>
-              </span>
-            </p>
-          </div>
-          <div className="text-block skills">
-            <h3 style={h3BorderBottom}>Skills</h3>
-            <h4>Coding</h4>
-            <ul className="clean-list">
-              <li>Bash</li>
-              <li>CSS</li>
-              <li>HTML</li>
-              <li>JavaScipt</li>
-              <li>jQuery</li>
-              <li>PHP</li>
-              <li>PowerShell</li>
-              <li>Python</li>
-              <li>React</li>
-            </ul>
-            <h4>Databases</h4>
-            <ul className="clean-list">
-              <li>MongoDB</li>
-              <li>Postgres</li>
-              <li>SQL</li>
-            </ul>
-            <h4>Dev Tools</h4>
-            <ul className="clean-list">
-              <li>AWS EC2</li>
-              <li>AWS Patch Manager</li>
-              <li>AWS Route 53</li>
-              <li>Docker</li>
-            </ul>
-            <h4>Sys Admin</h4>
-            <ul className="clean-list">
-              <li>Automox</li>
-              <li>Azure Active Directory</li>
-              <li>Debian &amp; Ubuntu</li>
-              <li>Intune</li>
-              <li>SonicWall</li>
-            </ul>
-          </div>
-          <div className="text-block hobbies" data-html2canvas-ignore="true">
-            <h3 style={h3BorderBottom}>Hobbies</h3>
-            <ul className="clean-list">
-              <li>3D Printing</li>
-              <li>Biking</li>
-              <li>Board Games</li>
-              <li>Dungeons &amp; Dragons</li>
-              <li>Hiking</li>
-              <li>Reading</li>
-            </ul>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+						<ul>
+							<li>
+								Front end and back end developer using PHP, JavaScript, CSS, and
+								HTML.
+							</li>
+							<li>Developed user interfaces in Objective C for iOS</li>
+							<li>Met with key stakeholders to document needs for projects</li>
+							<li>Worked on 3+ projects simultaneously</li>
+							<li>Designed and implemented complex user interfaces</li>
+						</ul>
+					</div>
+				</div>
+				<div className="right-col col-4">
+					<div className="text-block">
+						<h3 style={h3BorderBottom}>Education</h3>
+						<p>Bachelors in New Media</p>
+						<p>Minor in Computer Science, Film &amp; Video</p>
+						<p>
+							<img src={umobear} width="15" alt="UMO logo" /> University of
+							Maine
+						</p>
+						<p>
+							<span>
+								<FontAwesomeIcon icon={faLocationDot} /> <span> Orono, ME</span>
+							</span>
+						</p>
+					</div>
+					<div className="text-block skills">
+						<h3 style={h3BorderBottom}>Skills</h3>
+						<h4>Coding</h4>
+						<ul className="clean-list">
+							<li>Bash</li>
+							<li>CSS</li>
+							<li>HTML</li>
+							<li>JavaScipt</li>
+							<li>jQuery</li>
+							<li>PHP</li>
+							<li>PowerShell</li>
+							<li>Python</li>
+							<li>React</li>
+						</ul>
+						<h4>Databases</h4>
+						<ul className="clean-list">
+							<li>MongoDB</li>
+							<li>Postgres</li>
+							<li>SQL</li>
+						</ul>
+						<h4>Dev Tools</h4>
+						<ul className="clean-list">
+							<li>AWS EC2</li>
+							<li>AWS Patch Manager</li>
+							<li>AWS Route 53</li>
+							<li>Docker</li>
+						</ul>
+						<h4>Sys Admin</h4>
+						<ul className="clean-list">
+							<li>Automox</li>
+							<li>Azure Active Directory</li>
+							<li>Debian &amp; Ubuntu</li>
+							<li>Intune</li>
+							<li>SonicWall</li>
+						</ul>
+					</div>
+					<div className="text-block hobbies" data-html2canvas-ignore="true">
+						<h3 style={h3BorderBottom}>Hobbies</h3>
+						<ul className="clean-list">
+							<li>3D Printing</li>
+							<li>Biking</li>
+							<li>Board Games</li>
+							<li>Dungeons &amp; Dragons</li>
+							<li>Hiking</li>
+							<li>Reading</li>
+						</ul>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
